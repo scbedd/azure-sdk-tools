a03d83f5e80d55e1d441bb3caef301611e347da8
scbedd <45376673+scbedd@users.noreply.github.com>
2019-02-22 14:08:59 -0800

    adding license comments everywhere

--- packages/python-packages/doc-warden/warden/cmd_entry.py
+++ packages/python-packages/doc-warden/warden/cmd_entry.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
 from __future__ import print_function
 
 from .enforce_readme_presence import find_missing_readmes
--- packages/python-packages/doc-warden/warden/enforce_readme_presence.py
+++ packages/python-packages/doc-warden/warden/enforce_readme_presence.py
@@ -1,8 +1,9 @@
-# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project 
-# root for license information.
-# ------------------------------------------------------------------------------
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
 from __future__ import print_function
 
 import pathlib
