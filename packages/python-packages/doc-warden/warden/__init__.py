e94f2d667155d10c27d3d6150e12c6d64b28f2f6
scbedd <45376673+scbedd@users.noreply.github.com>
2019-02-22 13:36:23 -0800

    removing a bunch of stuff from the public namespace

--- packages/python-packages/doc-warden/warden/__init__.py
+++ packages/python-packages/doc-warden/warden/__init__.py
@@ -1,7 +1,7 @@
 from .version import VERSION
 
-from .enforce_readme_presence import *
-from .enforce_readme_content import *
+from .enforce_readme_presence import find_missing_readmes
+from .enforce_readme_content import verify_readme_content
 
 from .WardenConfiguration import WardenConfiguration
 from .warden_common import check_match, walk_directory_for_pattern, return_true, get_omitted_files
@@ -9,37 +9,12 @@ from .cmd_entry import console_entry_point
 
 
 __all__ = [
-           # configuration
            'WardenConfiguration',
-
-           # enforce readme presence
-           'DEFAULT_LOCATION',
-           'unrecognized_option',
            'find_missing_readmes',
-           'results',
-           'check_package_readmes',
-           'check_python_readmes',
-           'check_js_readmes',
-           'check_net_readmes',
-           'is_net_csproj_package',
-           'check_java_readmes',
-           'is_java_pom_package_pom',
-           'check_repo_root',
-           'find_alongside_file',
-           'get_file_sets',
-           'parse_pom',
-
-           # enforce readme content
            'verify_readme_content',
-
-           # console entry
            'console_entry_point',
-
-           # common
            'walk_directory_for_pattern',
            'get_omitted_files',
-           'check_match',
-           'return_true'
            ]
 
 __version__ = VERSION
