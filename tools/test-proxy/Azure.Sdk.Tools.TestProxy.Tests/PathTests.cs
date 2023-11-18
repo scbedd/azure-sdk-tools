6f4c701bf143c9bf1b75b2bbab6e3e9140067e55
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-11-17 16:05:30 -0800

    save progress

---
+++ tools/test-proxy/Azure.Sdk.Tools.TestProxy.Tests/PathTests.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure.Sdk.Tools.TestProxy.Tests
+{
+    public class PathTests
+    {
+
+    }
+}
